11bfc2dda860d052bbc3ce87ddfdf7b3e4f84471
Joe Chesney <joedihchesney@gmail.com>
2019-08-05 21:17:22 -0500

    changed the image names so theyll be in order when i add them using js instead of html

---
+++ js/carousel_pics.js
@@ -0,0 +1,16 @@
+{/* <div class="item active">
+  <img src="./images/slideshow_1.jpg" alt="live band photo" width="1200" height="700">
+  <div class="carousel-caption">
+    <!-- <h3>July 4th, 2016</h3> -->
+    <p>Photo by Monica Murray, 2019</p>
+  </div>
+</div> */}
+
+const carouselImageLocations = [
+  './images/slideshow_1.jpg',
+  './images/slideshow_2.jpg',
+  './images/slideshow_3.jpg',
+  './images/slideshow_4.jpg',
+  './images/slideshow_5.jpg',
+  './images/slideshow_6.jpg',
+]
